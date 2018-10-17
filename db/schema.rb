77d772baca51aa280877fa7c3d5c491b3e64d599
Howard DeVennish <howarddevennish@gmail.com>
2018-10-17 15:54:03 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181017154807) do
+ActiveRecord::Schema.define(version: 20181017155326) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "hometown"
+    t.string "hair_color"
   end
 
   create_table "networks", force: :cascade do |t|
